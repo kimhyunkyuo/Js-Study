3d6032a804280e81597bd55bfe5d573917cc9010
kimhyunkyuo <sdr04076@naver.com>
2024-07-11 11:16:30 +0900

    inita-commit

---
+++ 04-구문.js
@@ -0,0 +1,23 @@
+//구문
+
+//구조분해 할당 
+//객체, 배열을 인수로 받아 변수를 선언하고 값을 할당
+
+const arr1 = [1,2,3];
+
+function sum(a,b,c) {
+console.log(a+b+c)
+}
+sum(1,2,3);
+
+//구조분해
+const arr= [1,2,3];
+
+// const a = arr[0];
+// const b = arr[1];
+// const c = arr[2];
+const[a,b,c] = arr;
+
+
+
+console.log(a,b,c);
